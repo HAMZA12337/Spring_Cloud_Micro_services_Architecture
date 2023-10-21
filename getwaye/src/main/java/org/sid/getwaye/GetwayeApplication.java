6860793b88a7d19bbd723103319626f112ca98e7
Your Name <you@example.com>
2023-10-21 23:59:28 +0100

    fixed eurak disc prob

--- getwaye/src/main/java/org/sid/getwaye/GetwayeApplication.java
+++ getwaye/src/main/java/org/sid/getwaye/GetwayeApplication.java
@@ -2,6 +2,9 @@ package org.sid.getwaye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,20 +18,24 @@ public class GetwayeApplication {
 
 
 	// use java config instead of using application.yml
+//	@Bean
+//	RouteLocator routeLocator(RouteLocatorBuilder builder){
+//
+//		return builder.routes()
+//				.route(r1 -> r1.path("/customers/**")
+//						.uri("lb://CUSTOMER-SERVICE"))
+//
+//				.route(r2 -> r2.path("/products/**")
+//						.uri("lb://PRODUCT-SERVICE"))
+//
+//				.build();
+//
+//	}
+
 	@Bean
-	RouteLocator routeLocator(RouteLocatorBuilder builder){
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
 
-		return builder.routes()
-				.route(r1 -> r1.path("/customers/**")
-						.uri("http://localhost:8081/"))
-
-				.route(r2 -> r2.path("/products/**")
-						.uri("http://localhost:8082/"))
-
-				.build();
-
-	}
-
+		return new DiscoveryClientRouteDefinitionLocator(rdc,properties);}
 
 
 
